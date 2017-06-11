912c9fb01544f68f95bfb508d3a37aff1c77b90e
Jhin12 <lorenzo.bartozzi@libero.it>
2017-06-11 22:11:47 +0200

    Create login.js

--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -256,9 +256,6 @@ var callback_6 = function(req, res, next) {
         },
         body: requestData
     }, function(error, response, body) {
-        //console.log('error:', error); // Print the error if one occurred
-        //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        //  console.log('body:', body); // Print the HTML for the Google homepage
         if (!error && response.statusCode == 200) {
             var profile = {
                 id: req.ID
